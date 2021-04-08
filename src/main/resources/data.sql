75cfcb53a6fb56a843ef854b02116e70f66d572c
BRUNO PEDRONI <bruno.pedroni@etec.sp.gov.br>
2021-04-07 23:04:12 -0300

    finalização de conexão

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO tb_login (login, password, tipo, nome) VALUES ('chico', '123','a','Francisco');
 INSERT INTO tb_login (login, password, tipo, nome) VALUES ('xpto', '123','p','Joao');
 INSERT INTO tb_login (login, password, tipo, nome) VALUES ('xpto1', '123','a','Maria');
-INSERT INTO tb_login (login, password, tipo, nome) VALUES ('xpto2', '123','a','José');
+INSERT INTO tb_login (login, password, tipo, nome) VALUES ('xpto2', '1234555','a','José');
